2b6b3fe189ce6adc35d2792a6435b7d1c2f760c8
BLAES Clémence <clemence.blaes@laposte.net>
2020-08-23 21:23:34 +0200

    Add store.js via Upload
    This is the file that regroup all global variables for the game.

---
+++ js/store.js
@@ -0,0 +1,11 @@
+class Store {
+
+    static player1Played = null;
+    static player2Played = null;
+    static player1Choice = null;
+    static player2Choice = null;
+    static player1 = null;
+    static player2 = null;
+    static target = null;
+    static agressor = null;
+}
